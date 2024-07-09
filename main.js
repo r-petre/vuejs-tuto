c1ac1de0cebdf954c1012b50375ebf98956a37a8
Remy P <remy.petre@gmail.com>
2024-07-09 21:34:46 +0200

    add css + completed lesson 4

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             description: 'Best socks ever, for the coldest winters to the warmest summers',
-            image: './assets/images/green_sockets.png'
+            image: './assets/images/green_sockets.png',
+            // inStock: true
+            inventory: 8,
+            onSale: false
         }
     }
 })

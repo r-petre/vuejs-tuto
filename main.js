9a3c28f8ea44fb5133a0b7ba0a8b3c768e95a611
Rémy PÊTRE <rpetre@cbtw.tech>
2024-07-09 20:49:39 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            description: 'Best socks ever, for the coldest winters to the warmest summers',
+            image: './assets/images/green_sockets.png'
+        }
+    }
+})
